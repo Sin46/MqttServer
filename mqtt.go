8cc5b20d8357d1006a8289c586aa631d4d6c86c7
hinker <imgcre@outlook.com>
2021-03-04 17:17:29 +0800

    (refactor)

--- mqtt.go
+++ mqtt.go
@@ -1,69 +1,61 @@
 package mqtt
 
 import (
-	"fmt"
-	"os"
 	"time"
+
 	MQTT "github.com/eclipse/paho.mqtt.golang"
 )
 
-type MqttInfo struct {
-	CLIENT_ID string
-    BROKER_URL string
-	USERNAME string
-	PASSWORD string
-	KEEPALIVE int
+type Client struct {
+	base    MQTT.Client
+	in, out chan Msg
 }
 
-type MqttSub struct{
-    Enable bool
-    Topic string
+type Msg struct {
+	Topic, Payload string
 }
 
-type MqttMsg struct{
-    Topic,  Payload string
+type ClientInfo struct {
+	ClientId  string
+	BrokerUrl string
+	Username  string
+	Password  string
+	KeepAlive int
 }
 
-//整成一个协程,通过 chan 来
-func Sub(c MQTT.Client, sub_ch chan MqttSub) {
-	for {
-		x := <-sub_ch
-		fmt.Println("Sub")
-		if x.Enable {
-			if token := c.Subscribe(x.Topic, 0, nil); token.Wait() && token.Error() != nil {
-				fmt.Println(token.Error())
-				os.Exit(1)
-			}			
-		}else {
-			if token := c.Unsubscribe(x.Topic); token.Wait() && token.Error() != nil {
-				fmt.Println(token.Error())
-				os.Exit(1)
-			}	
-		}
-
-	}
-}
-
-func Server(cg MqttInfo, f MQTT.MessageHandler, pub_ch chan MqttMsg, sub_ch chan MqttSub){
-
-	fmt.Println("Init Mqtt Successfully:",cg)
-	opts := MQTT.NewClientOptions().AddBroker("tcp://"+cg.BROKER_URL).SetClientID(cg.CLIENT_ID)
-	opts.SetKeepAlive(2 * time.Second)
-	opts.SetUsername(cg.USERNAME)
-	opts.SetPassword(cg.PASSWORD)
-	opts.SetDefaultPublishHandler(f)
-	opts.SetPingTimeout(1 * time.Second)
-
-	c := MQTT.NewClient(opts)
-	if token := c.Connect(); token.Wait() && token.Error() != nil {
+func newClient(info ClientInfo) *Client {
+	//fmt.Println("Init Mqtt Successfully:", cg)
+	client := &Client{}
+	client.out = make(chan Msg)
+	client.in = make(chan Msg)
+	client.base = MQTT.NewClient(MQTT.NewClientOptions().
+		AddBroker("tcp://" + info.BrokerUrl).
+		SetClientID(info.ClientId).
+		SetKeepAlive(time.Duration(info.KeepAlive) * time.Second).
+		SetUsername(info.Username).
+		SetPassword(info.Password).
+		SetDefaultPublishHandler(func(c MQTT.Client, m MQTT.Message) {
+			client.out <- Msg{
+				Topic:   m.Topic(),
+				Payload: string(m.Payload()[:]),
+			}
+		}).
+		SetPingTimeout(1 * time.Second))
+	if token := client.base.Connect(); token.Wait() && token.Error() != nil {
 		panic(token.Error())
 	}
 
-	go Sub(c, sub_ch)
-	for {
-		x := <-pub_ch
-		//有多个生产者，但只有一个消费者
-		token := c.Publish(x.Topic, 0, false, x.Payload)
-		token.Wait()
+	go func() {
+		for msg := range client.in {
+			token := client.base.Publish(msg.Topic, 0, false, msg.Payload)
+			token.Wait()
+		}
+	}()
+	return client
+}
+
+func (client *Client) subscribe(topic string) {
+	if token := client.base.Subscribe(topic, 0, nil); token.Wait() && token.Error() != nil {
+		panic(token.Error())
 	}
 }
